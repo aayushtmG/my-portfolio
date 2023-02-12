b0dcdc68227f31f679565c0945748c176f90b2d6
iceTmZz <aiyoustmg@gmail.com>
2023-02-12 12:19:05 +0545

    updated

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,10 @@ export default function Home() {
           content="Aayush Tamang, Aayush Tamang portfolio,portoflio website,aayush portfolio,vercel"
         />
 
-        <meta name="keywords" content="aayush, tamang ,portfolio,website" />
+        <meta
+          name="keywords"
+          content="aayush, tamang ,portfolio,website,vercel, aayush tamang portfolio, aayush tamang vercel, aayush vercel portfolio"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
